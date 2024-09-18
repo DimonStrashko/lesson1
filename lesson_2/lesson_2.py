9d8f6031a60160d42b67ecd71994b02db047dc9b
Dimon <d80976872074@gmail.com>
2024-09-18 19:18:27 +0300

    друк на екрані в стовпчик

--- lesson_2/lesson_2.py
+++ lesson_2/lesson_2.py
@@ -1,3 +1,15 @@
-x = input("ввести четырехзначное число:")
-for y in x:
-    print(y)
+ Запитуємо у користувача 4-х значне число
+number = int(input("Введіть 4-х значне число: "))
+ # Отримуємо кожну цифру числа
+thousands, remainder = divmod(number, 1000)
+# тисячі
+hundreds, remainder = divmod(remainder, 100)
+# сотні
+tens, ones = divmod(remainder, 10)
+# десятки і одиниці
+
+    # Виводимо цифри в стовпчику
+print(thousands)
+print(hundreds)
+print(tens)
+print(ones)
