838ac170561dcf986167069ac8698b2044a26b85
DimonStrashko <d80976872074@gmail.com>
2024-10-20 20:00:20 +0300

    Виняток користувача

---
+++ lesson_14/Dz14.1.py
@@ -0,0 +1,66 @@
+# Користувацький виняток для перевищення кількості студентів
+class GroupOverflowException(Exception):
+    def __init__(self, message="Група не може містити більше 10 студентів"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class Human:
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}, {self.age} years old, Gender: {self.gender}'
+
+
+class Student(Human):
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f'Student: {super().__str__()}, Record Book: {self.record_book}'
+
+
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if len(self.group) >= 10:
+            raise GroupOverflowException()  # Викликаємо виняток, якщо більше 10 студентів
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+        else:
+            print(f"Студент з прізвищем {last_name} не знайдений")
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = '\n'.join(str(student) for student in self.group)
+        return f'Group Number: {self.number}\nStudents:\n{all_students}'
+
+
+# Приклади використання класів
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st3 = Student('Male', 28, 'Bill', 'Gates', 'AN143')
+
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+gr.add_student(st3)
+
+print(gr)
